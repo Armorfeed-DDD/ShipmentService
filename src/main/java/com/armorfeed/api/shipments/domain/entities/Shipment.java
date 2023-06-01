3d8ecbde4fd1896c61c4f900400da180c3f01c19
FranchescoSoto <u201821078@upc.edu.pe>
2023-06-01 14:45:57 -0500

    feature: add shipment entitie

---
+++ src/main/java/com/armorfeed/api/shipments/domain/entities/Shipment.java
@@ -0,0 +1,58 @@
+package com.armorfeed.api.shipments.domain.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@With
+@AllArgsConstructor
+@Table(name = "shipments")
+public class Shipment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String origin_city;
+
+    @Column(nullable = false)
+    private String destiny_city;
+
+    @Column(nullable = false)
+    private Date pick_up_date;
+
+    @Column(nullable = false)
+    private Date delivery_date;
+
+    @Column(nullable = false)
+    private Long status;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private String current_country;
+
+    @Column(nullable = false)
+    private String current_city;
+
+    @Column(nullable = false)
+    private Long latitude;
+
+    @Column(nullable = false)
+    private Long longitude;
+
+    @Column
+    private Long vehicle_id;
+
+    @Column(nullable = false)
+    private Long users_enterprise_id;
+
+    @Column(nullable = false)
+    private Long users_customer_id;
+}
