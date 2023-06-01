bcd0f16d885864e4cf45fe489b0b5732f65e9797
FranchescoSoto <u201821078@upc.edu.pe>
2023-06-01 17:08:27 -0500

    feature: add security

---
+++ src/main/java/com/armorfeed/api/shipments/security/TokenInterceptor.java
@@ -0,0 +1,38 @@
+package com.armorfeed.api.shipments.security;
+
+import com.armorfeed.api.shipments.providers.feignclients.UsersServiceFeignClient;
+import com.armorfeed.api.shipments.providers.feignclients.dtos.AuthTokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TokenInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UsersServiceFeignClient usersServiceFeignClient;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        if (!isValidToken(request)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+                
+        return true;
+    }
+
+    private boolean isValidToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            AuthTokenResponse authTokenResponse = usersServiceFeignClient.validateToken(token);
+
+            return authTokenResponse.isValidToken();
+        }
+        return false; // Token no válido o ausente
+    }
+    
+}
---
+++ src/main/java/com/armorfeed/api/shipments/security/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.armorfeed.api.shipments.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    HandlerInterceptor tokenInterceptor() {
+        return new TokenInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor());
+    }
+}
