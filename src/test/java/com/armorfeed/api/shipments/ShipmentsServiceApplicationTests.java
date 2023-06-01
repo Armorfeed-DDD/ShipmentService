e12207da6e1ac113f3b8ac67661d2f1dfee0ed60
FranchescoSoto <u201821078@upc.edu.pe>
2023-06-01 14:17:12 -0500

    add: dependencies

---
+++ src/test/java/com/armorfeed/api/shipments/ShipmentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armorfeed.api.shipments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
