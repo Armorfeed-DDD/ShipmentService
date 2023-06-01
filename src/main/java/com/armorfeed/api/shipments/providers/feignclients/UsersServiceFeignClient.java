e26f8f19db2f2139b548f2b2d87b075c3e219dba
FranchescoSoto <u201821078@upc.edu.pe>
2023-06-01 17:05:37 -0500

    feature: add feign client

---
+++ src/main/java/com/armorfeed/api/shipments/providers/feignclients/UsersServiceFeignClient.java
@@ -0,0 +1,17 @@
+package com.armorfeed.api.shipments.providers.feignclients;
+
+import com.armorfeed.api.shipments.providers.feignclients.dtos.AuthTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "users-service", url = "http://localhost:8090")
+public interface UsersServiceFeignClient {
+    @GetMapping("/api/v1/users/auth/validate-token/{token}")
+    public AuthTokenResponse validateToken(@PathVariable("token") String token);
+
+    @GetMapping("/api/v1/users/auth/validate-enterprise/{enterpriseId}")
+    public boolean validateEnterprise(@PathVariable("enterpriseId") Long enterpriseId);
+
+
+}
