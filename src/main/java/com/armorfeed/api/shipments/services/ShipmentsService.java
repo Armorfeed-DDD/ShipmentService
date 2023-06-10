23a9051b7bb92f5569b7a3baf3e0d350ae4d09b4
Samuel Maucaille <antonio0230@hotmail.com>
2023-06-09 22:07:31 -0500

    feat: added delete method for shipment.

--- src/main/java/com/armorfeed/api/shipments/services/ShipmentsService.java
+++ src/main/java/com/armorfeed/api/shipments/services/ShipmentsService.java
@@ -4,6 +4,8 @@ import com.armorfeed.api.shipments.domain.entities.Shipment;
 import com.armorfeed.api.shipments.repositories.ShipmentRepository;
 import com.armorfeed.api.shipments.shared.mapping.EnhancedModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +16,17 @@ public class ShipmentsService {
     @Autowired
     EnhancedModelMapper enhancedModelMapper;
 
+    public ResponseEntity<String> deleteShipment(Long shipmentId) {
+        if(shipmentRepository.findById(shipmentId).isPresent()){
+            shipmentRepository.deleteById(shipmentId);
+            return ResponseEntity.ok("Shipment Eliminated");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Shipment with the given id was not found");
+        }
+    }
+
     public void Save(Shipment shipment) { shipmentRepository.save(shipment);}
+
+    
 }
